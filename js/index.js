045af018085de0300d886bfd3a3fcdad19667fbb
illusiondelusion <kseniaschrodinger@gmail.com>
2022-07-28 01:39:25 +0300

    feat: add slick slider

--- js/index.js
+++ js/index.js
@@ -5,10 +5,35 @@ hamburger.addEventListener('click', function() {
     this.classList.toggle('is-active');
     nav.classList.toggle('nav-open');
   });
-  
+
 nav.addEventListener('click', function(event) {
       if (event.target.classList.contains('nav-link')) {
         hamburger.classList.remove('is-active');
         nav.classList.remove('nav-open');
       }
   });
+
+$(document).ready(function() {
+  $('.slider').slick({
+    slidesToShow: 3,
+    speed: 600,
+    infinite: true,
+    touchTreshold: 10,
+    variableWidth: true,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1090,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+});
